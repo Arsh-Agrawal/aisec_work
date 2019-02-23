09a8fa581b89f867eec27d5f848d394b37328fd6
Arsh-Agrawal <arsh.167.agrawal@gmail.com>
2019-02-23 15:50:47 +0530

    backend connection to frontend done

--- aisec_work/routes/index.js
+++ aisec_work/routes/index.js
@@ -3,12 +3,12 @@ const router =  require("express").Router();
 const users = require("./users.js");
 const paper = require("./quiz.js");
 
-router.post('/login.html',users.StudentLogin);
+router.post('/StudentLogin',users.StudentLogin);
 
 router.post('/AdminLogin',users.AdminLogin);
 
-router.post('/register.html',users.registration);
+router.post('/registration',users.registration);
 
-router.post('/route_here',paper.quiz);
+router.post('/quiz',paper.quiz);
 
 module.exports = router;
