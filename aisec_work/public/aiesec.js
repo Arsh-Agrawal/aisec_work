bdb61f896f61d8d7bc64b5ca5c679389512fb1d0
Xenos5282 <manas.ladha@yahoo.in>
2019-02-23 15:38:17 +0530

    updated js

--- aisec_work/public/aiesec.js
+++ aisec_work/public/aiesec.js
@@ -1,9 +1,35 @@
 
 var submit=document.querySelector("#sub");
-var hour=60*60*1000;
+var sec=1*1000
+var minute=60*sec
+var hour=60*minute
 
 setTimeout('Sub()',hour);
 
 function Sub(){
 	document.querySelector("form").submit();
 }
+
+var cards = $(".ques");
+for(var i = 0; i < cards.length; i++){
+    var target = Math.floor(Math.random() * cards.length -1) + 1;
+    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+    cards.eq(target).before(cards.eq(target2));
+}
+
+$(".ques").each(function(){
+	var cards = $(this).children(".custom-radio");
+	for(var i = 0; i < cards.length; i++){
+	    var target = Math.floor(Math.random() * cards.length -1) + 1;
+	    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+	    cards.eq(target).before(cards.eq(target2));
+}
+});
+$(".fillblank").each(function(){
+	var cards = $(this).children();
+	for(var i = 0; i < cards.length; i++){
+	    var target = Math.floor(Math.random() * cards.length -1) + 1;
+	    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+	    cards.eq(target).before(cards.eq(target2));
+}
+});
